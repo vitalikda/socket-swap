7270b91cb5e3d26bba8f3d3f638cdaacd0fc07cc
Vitaly <vitalikd13@gmail.com>
2025-04-27 13:08:56 +0300

    feat: enable gnosis/safe wallet

--- next.config.js
+++ next.config.js
@@ -2,8 +2,28 @@
 const nextConfig = {
   reactStrictMode: true,
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.externals.push("pino-pretty", "lokijs", "encoding");
     return config;
+  },
+  headers: async () => {
+    return [
+      {
+        // Adding CORS headers for /manifest.json
+        source: "/manifest.json",
+        locale: false,
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "https://app.safe.global",
+          },
+          { key: "Access-Control-Allow-Methods", value: "GET" },
+          {
+            key: "Access-Control-Allow-Headers",
+            value: "X-Requested-With, content-type, Authorization",
+          },
+        ],
+      },
+    ];
   },
 };
 
